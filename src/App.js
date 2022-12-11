e577abed2df495bc1beb8a8f68ad4fa60c640209
bishwaarun <yatrasansar@gmail.com>
2022-12-11 18:55:18 +0545

    Enable dark and light mode on nav bar and text form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
+import { useState } from 'react';
 // import About from './components/About'
 function App() {
+
+  const [enableMode, setEnableMode] = useState('light')
+
+  const toggleModeEnable = ()=>{
+    if (enableMode === 'light') {
+      setEnableMode('dark')
+      document.body.style.backgroundColor='#565776';
+    } else {
+      setEnableMode('light')
+      document.body.style.backgroundColor='white';
+    }
+    
+  }
+
+
   return (
     <>
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={enableMode} toggleMode={toggleModeEnable}/>
 
       <div className="container my-3">
-        <TextForm heading="Enter text to analyze below" />
+        <TextForm heading="Enter text to analyze below" mode={enableMode} />
         {/* <About/> */}
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -36,24 +36,24 @@ export default function TextForm(props) {
     //text ="new value"; - Wrong way to change the state
     // setText("new value"); - Correct way to change the state
     return (
-        <div>
+        <div style={{color:props.mode==="light"?"black":"white"}}>
             <h1>{props.heading}</h1>
-            <div className="container">
+            <div className="container" style={{color:props.mode==="light"?"black":"white"}}>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} style={{backgroundColor:props.mode==="light"?"white":"grey",color:props.mode==="light"?"black":"white"}}  onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div>
-                <button type="button" className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-                <button type="button" className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
-                <button type="button" className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
-                <button type="button" className="btn btn-primary mx-1" onClick={handleCopyClick}>Copy Text</button>
-                <button type="button" className="btn btn-primary mx-1" onClick={handleExtraSpaceClick}>Remove Extra Text</button>
+                <button type="button" className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button type="button" className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button type="button" className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+                <button type="button" className="btn btn-primary mx-1 my-1" onClick={handleCopyClick}>Copy Text</button>
+                <button type="button" className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaceClick}>Remove Extra Text</button>
             </div>
             <div className="container my-4">
                 <h2>Your Text Summary</h2>
                 <p>{text.split(' ').length} words and {text.length} characters </p>
                 <p>{text.split(' ').length * 0.008} minutes read</p>
                 <h2>Preview </h2>
-                <p>{text} </p>
+                <p>{text.length>0?text:"Enter something on above box to preview the text"} </p>
             </div>
 
         </div>
